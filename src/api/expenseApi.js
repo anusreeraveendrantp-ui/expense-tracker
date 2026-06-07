317f0e026850bb288caff25a959809e2561d2254
Anusree T P <anusreetp@Anusrees-MacBook-Air.local>
2026-06-07 13:20:53 +0530

    new change

--- src/api/expenseApi.js
+++ src/api/expenseApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://expense-tracker-1-qlur.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
